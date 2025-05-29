3368265e84dcd5700cb8e56bef8b755eb50b312a
Dias Zhexengaliyev <zhexengaliev05@gmail.com>
2025-05-30 01:08:35 +0500

    login

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
-// app.js
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const path = require('path');  // Added for static file serving
-const studentRoutes = require('./routes/studentRoutes');  // Routes for students
+const session = require('express-session');
+const path = require('path');
+const dotenv = require('dotenv');
+
+const studentRoutes = require('./routes/studentRoutes');
+const authRoutes = require('./routes/authRoutes');
+
+dotenv.config();
 
 const app = express();
 
 // Middlewares
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
 
-// Serve the static HTML file
-app.use(express.static(path.join(__dirname, 'views')));
+// Session setup
+app.use(session({
+  secret: 'alphaSecretKey',
+  resave: false,
+  saveUninitialized: false
+}));
 
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// View engine setup
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Routes
-app.use('/api/students', studentRoutes);  // API routes for students
+app.use('/api/students', studentRoutes);
+app.use('/', authRoutes);
 
-// Start the server
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+// Start server on PORT from env or 3000 (default)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server started on http://localhost:${PORT}`);
 });
---
+++ routes/authRoutes.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../config/db');
+
+router.get('/login', (req, res) => {
+  res.render('login', { error: null });
+});
+router.get('/', (req, res) => {
+  res.render('login', { error: null });
+});
+
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+    const user = result.rows[0];
+
+    if (!user || password !== user.password_hash) {
+      return res.render('login', { error: 'Неверный email или пароль' });
+    }
+
+
+    req.session.user = {
+      id: user.id,
+      email: user.email,
+      role: user.role
+    };
+
+    console.log(`✅ Вход: ${user.email}`);
+    res.redirect('/index');
+
+  } catch (err) {
+    console.error('❌ Ошибка при входе:', err);
+    res.status(500).send('Внутренняя ошибка сервера');
+  }
+});
+
+router.get('/index', (req, res) => {
+  if (!req.session.user) {
+    return res.redirect('/login');
+  }
+  res.render('index', { user: req.session.user });
+});
+
+// Выход
+router.get('/logout', (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/login');
+  });
+});
+
+module.exports = router;
